4031d2d9e86ecb0845b43ce3c45629c2ba2b7c30
satomm1 <91570784+satomm1@users.noreply.github.com>
2023-06-03 10:59:10 -0700

    Modify for melt pool dataset

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 from PIL import Image
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+from skimage import io
+import os
+import torch
 
 
 class CIFAR10Pair(CIFAR10):
@@ -19,6 +24,42 @@ class CIFAR10Pair(CIFAR10):
             target = self.target_transform(target)
 
         return pos_1, pos_2, target
+
+
+class MeltpoolDataset(Dataset):
+    """Dataset for Meltpool Images and Process Parameters"""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with process parameters.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.data_frame = pd.read_excel(csv_file, sheet_name='Sheet1', header=None, engine='openpyxl')
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.data_frame.iloc[idx, 0])
+        image = io.imread(img_name)
+        image = Image.fromarray(image)
+        image = image.convert(mode="RGB")
+
+        if self.transform is not None:
+            pos_1 = self.transform(image)
+            pos_2 = self.transform(image)
+
+        return pos_1, pos_2
+
 
 
 train_transform = transforms.Compose([
